d7497447d29ecff97465bf102fa6c091fca34272
RADU Iulia-Teodora <iulia.radu10@yahoo.com>
2022-11-30 00:06:35 +0200

    generare sudoku rezolvabil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from random import randint, shuffle
+from functools import partial
 
 def check_grid(grid):
   for row in range(0,9):
@@ -7,29 +9,152 @@ def check_grid(grid):
           return False
   return True 
 
+def solve_grid(grid):
+  global counter
+  #Find next empty cell
+  for i in range(0,81):
+    row=i//9
+    col=i%9
+    if grid[row][col]==0:
+      for value in range (1,10):
+        #Check that this value has not already be used on this row
+        if not(value in grid[row]):
+          #Check that this value has not already be used on this column
+          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+            #Identify which of the 9 squares we are working on
+            square=[]
+            if row<3:
+              if col<3:
+                square=[grid[i][0:3] for i in range(0,3)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(0,3)]
+              else:  
+                square=[grid[i][6:9] for i in range(0,3)]
+            elif row<6:
+              if col<3:
+                square=[grid[i][0:3] for i in range(3,6)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(3,6)]
+              else:  
+                square=[grid[i][6:9] for i in range(3,6)]
+            else:
+              if col<3:
+                square=[grid[i][0:3] for i in range(6,9)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(6,9)]
+              else:  
+                square=[grid[i][6:9] for i in range(6,9)]
+            #Check that this value has not already be used on this 3x3 square
+            if not value in (square[0] + square[1] + square[2]):
+              grid[row][col]=value
+              if check_grid(grid):
+                counter+=1
+                break
+              else:
+                if solve_grid(grid):
+                  return True
+      break
+  grid[row][col]=0  
+
+def fill_grid(grid):
+  global counter
+  for i in range(0,81):
+    row=i//9
+    col=i%9
+    if grid[row][col]==0:
+      shuffle(lista_numere)      
+      for value in lista_numere:
+        #Check that this value has not already be used on this row
+        if not(value in grid[row]):
+          #Check that this value has not already be used on this column
+          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+            #Identify which of the 9 squares we are working on
+            square=[]
+            if row<3:
+              if col<3:
+                square=[grid[i][0:3] for i in range(0,3)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(0,3)]
+              else:  
+                square=[grid[i][6:9] for i in range(0,3)]
+            elif row<6:
+              if col<3:
+                square=[grid[i][0:3] for i in range(3,6)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(3,6)]
+              else:  
+                square=[grid[i][6:9] for i in range(3,6)]
+            else:
+              if col<3:
+                square=[grid[i][0:3] for i in range(6,9)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(6,9)]
+              else:  
+                square=[grid[i][6:9] for i in range(6,9)]
+            #Check that this value has not already be used on this 3x3 square
+            if not value in (square[0] + square[1] + square[2]):
+              grid[row][col]=value
+              if check_grid(grid):
+                return True
+              else:
+                if fill_grid(grid):
+                  return True
+      break
+  grid[row][col]=0           
+
 def generare_sudoku_rezolvabil():
+ global lista_numere, counter
  lista_numere=[1,2,3,4,5,6,7,8,9]
  grid = []
- grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
- grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
- grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
- grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
- grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
- grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
- grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
- grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
- grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ for i in range(9):
+  grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ fill_grid(grid)
+ print(grid)
+ attempts = 5 
+ counter=1
+ while attempts>0:
+  row = randint(0,8)
+  col = randint(0,8)
+  while grid[row][col]==0:
+    row = randint(0,8)
+    col = randint(0,8) 
+  backup = grid[row][col]
+  grid[row][col]=0
+  
+  #Take a full copy of the grid
+  copyGrid = []
+  for r in range(0,9):
+     copyGrid.append([])
+     for c in range(0,9):
+        copyGrid[r].append(grid[r][c])
+  
+  #Count the number of solutions that this grid has (using a backtracking approach implemented in the solveGrid() function)
+  counter=0      
+  solve_grid(copyGrid)   
+  #If the number of solution is different from 1 then we need to cancel the change by putting the value we took away back in the grid
+  if counter!=1:
+    grid[row][col]=backup
+    #We could stop here, but we can also have another attempt with a different cell just to try to remove more numbers
+    attempts -= 1
+ print(grid)
+  
 
-def start_game():
+def start_game(game_grid):
  generare_sudoku_rezolvabil()
+ game_grid.destroy()
+ game_grid = Tk()
+ game_grid.title("Sudoku")
+ l1 = Button(game_grid, text="Sudoku", width=10)
+ l1.grid(row=1, column=1)
  pass
 
 def start():
  window = Tk()
  window.title("Sudoku")
  window.geometry("500x500")
+ start = partial(start_game,window)
  label1 = Label(window, text="Sudoku", font="arial, 50",width=10, height=2, borderwidth=3, relief="flat")
- button_start = Button(window, text ="Start", command = start_game, width = 500, font = 'summer, 25', bd = 10)
+ button_start = Button(window, text ="Start", command = start, width = 500, font = 'summer, 25', bd = 10)
  label1.pack()
  button_start.place(relx=0.5, rely=0.5, anchor=CENTER)
  window.mainloop()
