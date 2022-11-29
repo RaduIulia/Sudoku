a18ea3790bae0a726fa64c2912e9c793a421e57d
RADU Iulia-Teodora <iulia.radu10@yahoo.com>
2022-11-29 16:05:52 +0200

    generare sudoku

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-import tkinter
 from tkinter import *
 
+def check_grid(grid):
+  for row in range(0,9):
+      for col in range(0,9):
+        if grid[row][col]==0:
+          return False
+  return True 
 
-def startgame():
-   pass
+def generare_sudoku_rezolvabil():
+ lista_numere=[1,2,3,4,5,6,7,8,9]
+ grid = []
+ grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+ grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-def initializari_grafice():
+def start_game():
+ generare_sudoku_rezolvabil()
+ pass
+
+def start():
  window = Tk()
  window.title("Sudoku")
  window.geometry("500x500")
  label1 = Label(window, text="Sudoku", font="arial, 50",width=10, height=2, borderwidth=3, relief="flat")
- button_start = Button(window, text ="Start", command = startgame, width = 500, font = 'summer, 25', bd = 10)
+ button_start = Button(window, text ="Start", command = start_game, width = 500, font = 'summer, 25', bd = 10)
  label1.pack()
  button_start.place(relx=0.5, rely=0.5, anchor=CENTER)
  window.mainloop()
 
 def sudoku():
- initializari_grafice()
- pass
+ start()
 
 def main():
  sudoku()
