5ae781b969e53a3f6a4ed6450b8831e1e9bdc6cc
RADU Iulia-Teodora <iulia.radu10@yahoo.com>
2022-12-07 22:56:36 +0200

    putin GUI, inceput meniu selectare dificultate si inceput timer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from random import randint, shuffle
-from functools import partial
+import time
+from tkinter import messagebox
 
 def check_grid(grid):
   for row in range(0,9):
@@ -132,28 +133,101 @@ def generare_sudoku_rezolvabil():
     attempts -= 1
  print(grid)
   
+def distruge(x):
+ x.destroy()
 
-def start_game(game_grid):
+def start_game():
  generare_sudoku_rezolvabil()
- game_grid.destroy()
  game_grid = Tk()
  game_grid.title("Sudoku")
- label1p = Label(game_grid, text="Sudoku Time Remaining:", font="arial, 50", borderwidth=3, relief="flat")
- for i in range(len(grid)):
-  for j in range(len(grid)):
-    e = Entry(width=5, fg='red',font=('Arial',15,'bold'))
-    e.grid(row=i, column=j)
-    e.insert(END,(grid[i][j]))
+ game_grid.geometry("500x500")
+ puzzle = Frame(game_grid, bg='white')
+ puzzle.pack()
+ hour=StringVar()
+ minute=StringVar()
+ second=StringVar()
+ hour.set("00")
+ minute.set("00")
+ second.set("00")
+ hourEntry= Entry(game_grid, width=3, font=("Arial",18,""),textvariable=hour)
+ minuteEntry= Entry(game_grid, width=3, font=("Arial",18,""),textvariable=minute)
+ secondEntry= Entry(game_grid, width=3, font=("Arial",18,""),textvariable=second)
+ label1p = Label(game_grid, text="Sudoku Time Remaining:", font="arial, 25", borderwidth=3, relief="flat")
+ blocks = [] 
+ for r in range(3):
+    row = []
+    for c in range(3): 
+        frame = Frame(puzzle, bd=1, highlightbackground='light blue', highlightcolor='light blue', highlightthickness=1)
+        frame.grid(row=r, column=c, sticky='nsew')
+        row.append(frame)
+    blocks.append(row)
+ btn_cells = [[None for x in range(9)] for x in range(9)]
+ for i in range(9):
+    for j in range(9):
+        frm_cell = Frame(blocks[i // 3][j // 3])
+        frm_cell.grid(row=(i % 3), column=(j % 3), sticky='nsew')
+        frm_cell.rowconfigure(0, minsize=50, weight=1)
+        frm_cell.columnconfigure(0, minsize=50, weight=1)
+        var = StringVar()
+        btn_cells[i][j] = Button(frm_cell, relief='ridge', bg='white', textvariable=var)
+        btn_cells[i][j].grid(sticky='nsew')
+        var.set(grid[i][j])
+ label1p.pack()
+ hourEntry.pack()
+ minuteEntry.pack()
+ secondEntry.pack()
+ temp = 0*3600 + 0*60 + 5
+ while temp >-1:
+        mins,secs = divmod(temp,60)
+  
+        # Converting the input entered in mins or secs to hours,
+        # mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
+        # 50min: 0sec)
+        hours=0
+        if mins >60:
+             
+            # divmod(firstvalue = temp//60, secondvalue
+            # = temp%60)
+            hours, mins = divmod(mins, 60)
+         
+        # using format () method to store the value up to
+        # two decimal places
+        hour.set("{0:2d}".format(hours))
+        minute.set("{0:2d}".format(mins))
+        second.set("{0:2d}".format(secs))
+  
+        # updating the GUI window after decrementing the
+        # temp value every time
+        game_grid.update()
+        time.sleep(1)
+  
+        # when temp value = 0; then a messagebox pop's up
+        # with a message:"Time's up"
+        if (temp == 0):
+            messagebox.showinfo("Sudoku Time Countdown", "Time's up, you've lost ")
+            distruge(game_grid)
+            start()
+         
+        # after every one sec the value of temp will be decremented
+        # by one
+        temp -= 1
 
 def start():
  window = Tk()
  window.title("Sudoku")
  window.geometry("500x500")
- start = partial(start_game,window)
  label1 = Label(window, text="Sudoku", font="arial, 50",width=10, height=2, borderwidth=3, relief="flat")
- button_start = Button(window, text ="Start", command = start, width = 500, font = 'summer, 25', bd = 10)
+ button_start = Button(window, text ="Start", command = lambda: [distruge(window),start_game()], width = 500, font = 'summer, 25', bd = 10)
+ label2= Label(window, text = "Select the difficulty below :  ", font = ("Times New Roman", 30), padx = 10, pady = 10)
+ list = Listbox(window, selectmode = "single")
+ x =["Easy", "Medium", "Hard"]
+ for each_item in range(len(x)):
+    list.insert(END, x[each_item])
+    list.itemconfig(each_item)
  label1.pack()
- button_start.place(relx=0.5, rely=0.5, anchor=CENTER)
+ button_start.pack()
+ label2.pack()
+ list.pack(padx = 10, pady = 10, fill = "both")
  window.mainloop()
 
 def sudoku():
