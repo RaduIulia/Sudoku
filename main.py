c33abf917e2d0f79017a6ec08846d1637e901e0a
RADU Iulia-Teodora <iulia.radu10@yahoo.com>
2022-12-09 18:58:10 +0200

    GUI sudoku ce primeste input

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from random import randint, shuffle
 import time
 from tkinter import messagebox
+from functools import partial
 
 def check_grid(grid):
   for row in range(0,9):
@@ -164,44 +165,63 @@ def buton_valabil(i,j):
 def switch(b):
     b["state"] = DISABLED
 
-def start_game():
+def add_label_timer(game_grid):
+ timer = Frame(game_grid, bg='white')
+ timer.pack()
+ timeLabel= Label(timer, font=("Arial",15,""),text="Sudoku Time Remaining: ")
+ timeLabel.pack(side=LEFT)
+ mins_box = Label( timer,font=("Arial",15,""),textvariable=minute)
+ mins_box.pack(side=LEFT)
+ timeLabel= Label(timer, font=("Arial",15,""),text=":")
+ timeLabel.pack(side=LEFT)
+ sec_box = Label(timer, font=("Arial",15,""),textvariable=second)
+ sec_box.pack(side=LEFT)
+ 
+def add_butoane(game_grid):
+ butoane = Frame(game_grid, bg='white')
+ butoane.pack()
+ button_clear = Button(butoane, text ="Clear",  width = 50, font = 'summer, 20', bd = 5)
+ button_clear.pack()
+ button_check = Button(butoane, text ="Check",  width = 50, font = 'summer, 20', bd = 5)
+ button_check.pack()
+
+def desenat_sudoku(puzzle):
+ board  = []
+ for i in range(1,10):
+           board += [[0,0,0,0,0,0,0,0,0]]
+ for row in range(9):
+  for col in range(9):
+   if (row < 3 or row > 5) and (col < 3 or col > 5):
+       color = 'white' 
+   elif (row >= 3 and row < 6) and (col >=3 and col < 6):
+      color = 'white'
+   else:
+      color = 'grey'
+   if grid[row][col] == 0:
+    board[row][col] = Entry(puzzle, width = 2, font = ('Arial', 20), bg = color, cursor = 'arrow', borderwidth = 2,
+                      highlightcolor = 'yellow', highlightthickness = 0, highlightbackground = 'black') 
+   else:
+    board[row][col] = Label(puzzle, width = 2, font = ('Arial', 20), bg = color, cursor = 'arrow', borderwidth = 2,
+                      highlightcolor = 'yellow', highlightthickness = 0, highlightbackground = 'black',text=grid[row][col]) 
+   board[row][col].grid(row = row, column = col)
+
+def start_game(game_grid):
  generare_sudoku_rezolvabil()
- global game_grid
+ game_grid.destroy()
  game_grid = Tk()
  game_grid.title("Sudoku")
  game_grid.geometry("500x500")
- puzzle = Frame(game_grid, bg='white')
- puzzle.pack()
- blocks = [] 
- for r in range(3):
-    row = []
-    for c in range(3): 
-        frame = Frame(puzzle, bd=1, highlightbackground='light blue', highlightcolor='light blue', highlightthickness=1)
-        frame.grid(row=r, column=c, sticky='nsew')
-        row.append(frame)
-    blocks.append(row)
- btn_cells = [[None for x in range(9)] for x in range(9)]
- for i in range(9):
-    for j in range(9):
-        frm_cell = Frame(blocks[i // 3][j // 3])
-        frm_cell.grid(row=(i % 3), column=(j % 3), sticky='nsew')
-        frm_cell.rowconfigure(0, minsize=50, weight=1)
-        frm_cell.columnconfigure(0, minsize=50, weight=1)
-        var = StringVar()
-        btn_cells[i][j] = Button(frm_cell, relief='ridge', bg='white', textvariable=var)
-        btn_cells[i][j].grid(sticky='nsew')
-        var.set(grid[i][j])
- timeLabel= Label(game_grid, font=("Arial",15,""),text="Sudoku Time Remaining: ")
- timeLabel.pack(side=LEFT,expand=YES)
+ global minute,second
  minute=StringVar()
  second=StringVar()
  setare_valori_timer(minute,second)
+ add_label_timer(game_grid)
+ puzzle = Frame(game_grid, bg='white')
+ puzzle.pack()
+ desenat_sudoku(puzzle)
+ add_butoane(game_grid)
  global temp
- while temp >-1:
-  minuteLabel= Label(game_grid, font=("Arial",15,""),textvariable=minute)
-  minuteLabel.pack(side=LEFT,expand=YES)
-  secondLabel= Label(game_grid, font=("Arial",15,""), textvariable=second)
-  secondLabel.pack(side=LEFT,expand=YES)
+ if temp >-1:
   mins,secs = divmod(temp,60)
   minute.set("{0:2d}".format(mins))
   second.set("{0:2d}".format(secs))
@@ -211,8 +231,7 @@ def start_game():
    if (temp == 0):
     messagebox.showinfo("Sudoku Time Countdown", "Time's up, you've lost!")
   temp -= 1
- 
-  game_grid.mainloop()
+ game_grid.mainloop()
 
 def setare_variabila_dificultate(x):
  global dificultate
@@ -246,9 +265,10 @@ def start_menu():
  global window
  window = Tk()
  window.title("Sudoku")
+ game = partial(start_game, window)
  window.geometry("500x500")
  label1 = Label(window, text="Sudoku", font="arial, 50",width=10, height=2, borderwidth=3, relief="flat")
- button_start = Button(window, text ="Start", command = lambda: [distruge(window),start_game()], width = 500, font = 'summer, 25', bd = 10)
+ button_start = Button(window, text ="Start", command = game, width = 500, font = 'summer, 25', bd = 10)
  label2= Label(window, text = "Select the difficulty below :  ", font = ("Times New Roman", 30), padx = 10, pady = 10)
  R1 = Radiobutton(window, text="Easy",background="white", activebackground="red",indicatoron = 0,font=30,command=update_label1)
  R2 = Radiobutton(window, text="Medium",background="white", activebackground="red",indicatoron = 0,font=30,command=update_label2)
